254861d3f0e7fafe4d56b74f8db36b94158d56ef
Saj Goonatilleke <sg@redu.cx>
2017-09-16 02:00:19 +1000

    glide.yaml: Vendor container-linux-config-transpiler v0.4.2

--- vendor/github.com/coreos/go-systemd/dbus/set.go
+++ vendor/github.com/coreos/go-systemd/dbus/set.go
@@ -1,4 +1,4 @@
-// Copyright 2016 CoreOS, Inc.
+// Copyright 2015 CoreOS, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,9 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package types
+package dbus
 
-type Timeouts struct {
-	HttpResponseHeaders *int `json:"httpResponseHeaders,omitempty"`
-	HttpTotal           *int `json:"httpTotal,omitempty"`
+type set struct {
+	data map[string]bool
+}
+
+func (s *set) Add(value string) {
+	s.data[value] = true
+}
+
+func (s *set) Remove(value string) {
+	delete(s.data, value)
+}
+
+func (s *set) Contains(value string) (exists bool) {
+	_, exists = s.data[value]
+	return
+}
+
+func (s *set) Length() int {
+	return len(s.data)
+}
+
+func (s *set) Values() (values []string) {
+	for val, _ := range s.data {
+		values = append(values, val)
+	}
+	return
+}
+
+func newSet() *set {
+	return &set{make(map[string]bool)}
 }
